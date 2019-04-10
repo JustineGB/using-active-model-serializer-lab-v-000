56d5ffde11b61fe123c328326e5e20019231d77e
Justine B <justinegayton@gmail.com>
2019-04-10 17:17:42 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,10 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    # respond_to do |format|
+    #   format.html { render :show }
+    #   format.json { render json: @product }
+    # end
   end
 
   def inventory
@@ -13,20 +17,20 @@ class ProductsController < ApplicationController
     render plain: product.description
   end
 
-  def new
-    @product = Product.new
-  end
-
   def create
     Product.create(product_params)
     redirect_to products_path
+  end
+  
+  def new
+    @product = Product.new
   end
 
   def show
     @product = Product.find(params[:id])
     respond_to do |format|
       format.html { render :show }
-      format.json { render json: @product.to_json(only: [:id, :name, :description, :price, :inventory])}
+      format.json { render json: @product }
     end
   end
 
